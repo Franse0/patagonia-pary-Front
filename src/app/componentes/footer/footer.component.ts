9c7f60e777a014cbee71bef0c6488eb651dfe472
Franse0 <panchymairn.fm@gmail.com>
2024-04-22 04:31:04 -0300

    22 de abil

--- src/app/componentes/footer/footer.component.ts
+++ src/app/componentes/footer/footer.component.ts
@@ -10,4 +10,8 @@ export class FooterComponent {
   mail(){
     alert("El contacto es patagoniapary@gmail.com")
   }
+  sumarme(event:Event){
+    event.preventDefault()
+    alert("Estamos trabajando en una interface grafica para volver esto un proceso mas simple. Mientras comunicate con el mail 'patagoniapary@gmail.com' para poder iniciar el proceso por medio de alguien del equipo")
+  }
 }
